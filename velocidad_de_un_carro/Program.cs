c1dd74198cbea612d0ce6bf81e9c2726507587cf
Gabriel Torres <elcomparosh97@gmail.com>
2024-02-08 18:04:14 -0600

    Agregar archivos de proyecto.

---
+++ velocidad_de_un_carro/Program.cs
@@ -0,0 +1,12 @@
+﻿double distance, time,vel;
+
+Console.WriteLine("What is the distance in km that your car traveled?");
+distance=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What was the time in hours in which your car traveled the previous distance?");
+time = Convert.ToDouble(Console.ReadLine());
+
+vel = distance / time;
+
+Console.WriteLine("Its speed is:" + vel + "km/h");
+Console.ReadKey();
